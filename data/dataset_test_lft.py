8145bcfe9f1adba7cba60d729d147fa4ca7d87a7
Aytac Özkan <aytac.oezkan@tu-berlin.de>
2024-04-24 10:37:13 +0200

    MRI data

---
+++ data/dataset_test_lft.py
@@ -0,0 +1,57 @@
+import os
+
+import h5py
+import numpy as np
+from torch.utils.data import DataLoader, Dataset
+from torch.utils.data.dataset import Dataset
+from torchvision.transforms import ToTensor
+
+
+def MultiTestSetDataLoader(args):
+    # get testdataloader of every test dataset
+    dataset_dir = args.path_for_test + 'SR_' + str(args.angRes) + 'x' + str(args.angRes) + '_' + \
+                  str(args.scale_factor) + 'x/'
+    data_list = os.listdir(dataset_dir)
+
+    test_Loaders = []
+    length_of_tests = 0
+    for data_name in data_list:
+        test_Dataset = TestSetDataLoader(args, data_name)
+        length_of_tests += len(test_Dataset)
+
+        test_Loaders.append(DataLoader(dataset=test_Dataset, num_workers=args.num_workers, batch_size=1, shuffle=False))
+
+    return data_list, test_Loaders, length_of_tests
+
+class TestSetDataLoader(Dataset):
+    def __init__(self, args, data_name='ALL'):
+        super(TestSetDataLoader, self).__init__()
+        self.dataset_dir = args.path_for_test + 'SR_' + str(args.angRes) + 'x' + str(args.angRes) + '_' + \
+                           str(args.scale_factor) + 'x/'
+        self.data_list = [data_name]
+
+        self.file_list = []
+        for data_name in self.data_list:
+            tmp_list = os.listdir(self.dataset_dir + data_name)
+            for index, _ in enumerate(tmp_list):
+                tmp_list[index] = data_name + '/' + tmp_list[index]
+
+            self.file_list.extend(tmp_list)
+
+        self.item_num = len(self.file_list)
+
+    def __getitem__(self, index):
+        file_name = [self.dataset_dir + self.file_list[index]]
+        with h5py.File(file_name[0], 'r') as hf:
+            Lr_SAI_y = np.array(hf.get('Lr_SAI_y'))
+            Hr_SAI_y = np.array(hf.get('Hr_SAI_y'))
+            Lr_SAI_y = np.transpose(Lr_SAI_y, (1, 0))
+            Hr_SAI_y = np.transpose(Hr_SAI_y, (1, 0))
+
+        Lr_SAI_y = ToTensor()(Lr_SAI_y.copy())
+        Hr_SAI_y = ToTensor()(Hr_SAI_y.copy())
+
+        return Lr_SAI_y, Hr_SAI_y
+
+    def __len__(self):
+        return self.item_num
---
+++ test_mri_data.py
@@ -0,0 +1,33 @@
+import pathlib
+from data.subsample import RandomMaskFunc
+from data.transforms import to_tensor, apply_mask
+from data.mri_data import SliceDataset
+
+
+
+
+mask_func = RandomMaskFunc(
+    center_fractions=[0.08, 0.04],
+    accelerations=[4, 8]
+)
+
+def data_transform(kspace, mask, target, data_attributes, filename, slice_num):
+    # Transform the data into appropriate format
+    # Here we simply mask the k-space and return the result
+    kspace = to_tensor(kspace)
+    target = to_tensor(target)
+    # masked_kspace = apply_mask(kspace, mask_func)
+    return kspace,target
+
+dataset = SliceDataset(
+    root=pathlib.Path('/mnt/d/Medical/multicoil_train'),
+    transform=data_transform,
+    challenge='multicoil'
+)
+
+for kspace,target in dataset:
+    # Do reconstruction
+    pass
+
+
+
